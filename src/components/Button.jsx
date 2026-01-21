aac8ad2b83ef6789cad0d1923f19d4a4fe189797
Tassio Neves <tassions@hotmail.com>
2026-01-21 14:26:46 -0300

    feat: initial Task and Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+const Button = ({ children, variant = "primary" }) => {
+  const getVariantClasses = () => {
+    if (variant === "primary") {
+      return "bg-[#00ADB5]  text-white"
+    }
+    if (variant === "ghost") {
+      return "bg-transparent text-[#818181]"
+    }
+  }
+  return (
+    <button
+      className={`flex items-center gap-2 rounded-md px-3 py-1 font-semibold transition hover:opacity-50 ${getVariantClasses()} text-xs`}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,27 @@
-const Tasks = () => {
-  return <div>Tasks</div>;
-};
+import { PlusIcon } from "lucide-react"
+import { Trash2 } from "lucide-react"
 
-export default Tasks;
+import Button from "./Button"
+const Tasks = () => {
+  return (
+    <div className="w-full px-8 py-16">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-2 p-3 px-2">
+          <span className="text-xs text-[#00ADB5]">Minhas Tarefas</span>
+          <h2 className="tex-[#35383E] text-xl font-bold">Minhas Tarefas</h2>
+        </div>
+        <div className="flex items-center gap-4">
+          <Button variant="ghost">
+            Limpar tarefas <Trash2 />
+          </Button>
+          <Button>
+            Nova Tarefa <PlusIcon />
+          </Button>
+        </div>
+      </div>
+      <div className="h-56 w-full rounded bg-[#FFFFFF]"></div>
+    </div>
+  )
+}
+
+export default Tasks
