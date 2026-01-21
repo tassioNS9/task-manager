1211325b66a4fa6c4025eba2afb7a10b0cd5b120
Tassio Neves <tassions@hotmail.com>
2026-01-21 15:02:48 -0300

    feat: add separator between tasks

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,6 @@
 import { PlusIcon } from "lucide-react"
 import { Trash2 } from "lucide-react"
+import { CloudSun, Moon, Sun } from "lucide-react"
 
 import Button from "./Button"
 const Tasks = () => {
@@ -19,7 +20,23 @@ const Tasks = () => {
           </Button>
         </div>
       </div>
-      <div className="h-56 w-full rounded bg-[#FFFFFF]"></div>
+      <div className="flex h-56 flex-col rounded-xl bg-white p-6">
+        <div className="space-y-3">
+          <p className="flex items-center gap-2 border-b border-solid border-[#F4F4F5] pb-3 text-[#949C9F]">
+            <Sun /> Manhã
+          </p>
+        </div>
+        <div className="my-6 space-y-3">
+          <p className="flex items-center gap-2 border-b border-solid border-[#F4F4F5] pb-3 text-[#949C9F]">
+            <CloudSun /> Tarde
+          </p>
+        </div>
+        <div className="space-y-3">
+          <p className="flex items-center gap-2 border-b border-solid border-[#F4F4F5] pb-3 text-[#949C9F]">
+            <Moon /> Noite
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
