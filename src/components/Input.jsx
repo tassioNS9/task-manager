8c22653ad2f0f4ee6af5492b840ec50f1226d72d
Tassio Neves <tassions@hotmail.com>
2026-01-23 10:54:32 -0300

    feat: add Input and InputLabal component

---
+++ src/components/Input.jsx
@@ -0,0 +1,14 @@
+import InputLabel from "./InputLabel"
+const Input = ({ label, ...rest }) => {
+  return (
+    <div className="flex flex-col space-y-1 text-left">
+      <InputLabel htmlFor={rest.id}>{label}</InputLabel>
+      <input
+        className="border-brand-border outline-brand-primary rounded-lg border border-solid px-4 py-3 outline-[#00ADB5] placeholder:text-sm placeholder:text-[#9A9C9F]"
+        {...rest}
+      />
+    </div>
+  )
+}
+
+export default Input
---
+++ src/components/InputLabel.jsx
@@ -0,0 +1,9 @@
+const InputLabel = (props) => {
+  return (
+    <label className="text-brand-dark-blue text-sm font-semibold" {...props}>
+      {props.children}
+    </label>
+  )
+}
+
+export default InputLabel
