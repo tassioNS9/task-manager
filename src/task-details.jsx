229c6dd035973e118ffbeb81d67f3a203e1e29cb
Tassio Neves <tassions@hotmail.com>
2026-01-28 13:07:46 -0300

    feat: ensure task can be edited

--- src/task-details.jsx
+++ src/task-details.jsx
@@ -1,7 +1,7 @@
-import { CircleArrowLeft, Trash2 } from "lucide-react"
-import { useEffect, useState } from "react"
+import { CircleArrowLeft, Loader2Icon, Trash2 } from "lucide-react"
+import { useEffect, useRef, useState } from "react"
 import { Link, useParams } from "react-router-dom"
-import { Toaster } from "sonner"
+import { toast, Toaster } from "sonner"
 
 import Button from "./components/Button"
 import Input from "./components/Input"
@@ -10,6 +10,11 @@ import TimeSelect from "./components/TimeSelect"
 const TaskDetailsPage = () => {
   const { taskId } = useParams()
   const [task, setTask] = useState({})
+  const [time, setTime] = useState("")
+  const [errors, setErrors] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const titleRef = useRef()
+  const descriptionRef = useRef()
 
   useEffect(() => {
     const getTask = async () => {
@@ -17,11 +22,70 @@ const TaskDetailsPage = () => {
         method: "GET",
       })
       const data = await response.json()
-      console.log(data, "dadad")
       setTask(data)
     }
     getTask()
   }, [taskId])
+
+  const handleEditClick = async () => {
+    setIsLoading(true)
+    // Lógica para salvar as alterações da tarefa
+    const title = titleRef?.current.value
+    const description = descriptionRef?.current.value
+
+    // Nova Verificação utilizando o Ref
+    const newErrors = []
+    if (!title.trim()) {
+      newErrors.push({
+        inputName: "title",
+        message: "O campo Titulo é Obrigátorio!",
+      })
+    }
+    if (!time.trim()) {
+      newErrors.push({
+        inputName: "time",
+        message: "O campo de Horario é Obrigátorio!",
+      })
+    }
+    if (!description.trim()) {
+      newErrors.push({
+        inputName: "description",
+        message: "O campo Descrição é Obrigátorio!",
+      })
+    }
+    setErrors(newErrors)
+
+    if (newErrors.length > 0) return setIsLoading(false)
+
+    const task = {
+      title,
+      description,
+      time,
+    }
+    const response = await fetch(`http://localhost:3000/tasks/${taskId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(task),
+    })
+
+    if (!response.ok) {
+      // Tratar erro
+      return toast.error("Erro ao atualizar a tarefa.")
+    }
+    setIsLoading(false)
+    const updatedTask = await response.json()
+    setTask(updatedTask)
+    // Talvez mostrar uma notificação de sucesso
+    toast.success("Tarefa atualizada com sucesso!")
+  }
+
+  const titleError = errors.find((error) => error.inputName === "title")
+  const timeError = errors.find((error) => error.inputName === "time")
+  const descriptionError = errors.find(
+    (error) => error.inputName === "description"
+  )
 
   return (
     <div className="flex">
@@ -52,33 +116,51 @@ const TaskDetailsPage = () => {
             </h2>
           </div>
           <div className="flex items-center gap-4 self-end py-3">
-            <Button color="danger">
+            <Button disabled={isLoading} color="danger">
               Deletar Tarefa <Trash2 />
             </Button>
           </div>
         </div>
         <div className="flex flex-col rounded-xl bg-white p-6">
           <div className="space-y-3">
-            <Input id="title" label="Título" value={task?.title} />
+            <Input
+              id="title"
+              label="Título"
+              ref={titleRef}
+              defaultValue={task?.title}
+              errorMessage={titleError?.message}
+            />
           </div>
           <div className="my-6 space-y-3">
-            <TimeSelect value={task?.time} />
+            <TimeSelect
+              defaultValue={task?.time}
+              onChange={(e) => setTime(e.target.value)}
+              errorMessage={timeError?.message}
+            />
           </div>
           <div className="space-y-3">
             <Input
               id="description"
               label="Descrição"
-              value={task?.description}
+              defaultValue={task?.description}
+              ref={descriptionRef}
+              errorMessage={descriptionError?.message}
             />
           </div>
         </div>
 
         <div className="flex justify-end gap-3 py-3">
-          <Button size="large" color="secondary">
-            Cancelar
-          </Button>
-          <Button size="large" color="primary">
-            Salvar
+          <Button
+            disabled={isLoading}
+            onClick={handleEditClick}
+            size="large"
+            color="primary"
+          >
+            {isLoading ? (
+              <Loader2Icon className="animate-spin text-brand-text-gray" />
+            ) : (
+              "Salvar"
+            )}
           </Button>
         </div>
       </div>
