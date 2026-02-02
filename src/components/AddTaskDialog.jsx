1583f5dfcd7bac9d5e4a7e99383d39f5771d4b56
Tassio Neves <tassions@hotmail.com>
2026-02-02 16:23:11 -0300

    feat: add initial create task with react-hook-form

--- src/components/AddTaskDialog.jsx
+++ src/components/AddTaskDialog.jsx
@@ -4,6 +4,7 @@ import { Loader2Icon } from "lucide-react"
 import PropTypes from "prop-types"
 import { useEffect, useRef, useState } from "react"
 import { createPortal } from "react-dom"
+import { useForm } from "react-hook-form"
 import { CSSTransition } from "react-transition-group"
 import { toast } from "sonner"
 import { v4 } from "uuid"
@@ -13,55 +14,34 @@ import Input from "./Input"
 import TimeSelect from "./TimeSelect"
 const AddTaskDialog = ({ isOpen, handleClose, onAddTaskSucess }) => {
   const [time, setTime] = useState("evening")
-  const [errors, setErrors] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
   const nodeRef = useRef()
-  const titleRef = useRef()
-  const descriptionRef = useRef()
+  const {
+    register,
+    formState: { errors, isSubmitting },
+    handleSubmit,
+    reset,
+  } = useForm({
+    defaultValues: {
+      title: "",
+      time: "evening",
+      description: "",
+    },
+  })
 
   useEffect(() => {
     if (!isOpen) {
-      setTime("morning")
+      setTime("evening")
     }
   }, [isOpen])
 
-  const handleSaveClick = async () => {
-    const newErrors = []
-    const title = titleRef?.current.value
-    const description = descriptionRef?.current.value
-
-    // Nova Verificação utilizando o Ref
-    if (!title.trim()) {
-      newErrors.push({
-        inputName: "title",
-        message: "O campo Titulo é Obrigátorio!",
-      })
-    }
-    if (!time.trim()) {
-      newErrors.push({
-        inputName: "time",
-        message: "O campo de Horario é Obrigátorio!",
-      })
-    }
-    if (!description.trim()) {
-      newErrors.push({
-        inputName: "description",
-        message: "O campo Descrição é Obrigátorio!",
-      })
-    }
-    setErrors(newErrors)
-
-    if (newErrors.length > 0) return
-
+  const handleSaveClick = async (data) => {
     const task = {
       id: v4(),
-      title,
-      description,
+      title: data.title.trim(),
+      description: data.description.trim(),
       time,
       status: "not_started",
     }
-
-    setIsLoading(true)
     const response = await fetch("http://localhost:3000/tasks", {
       method: "POST",
       headers: {
@@ -69,21 +49,15 @@ const AddTaskDialog = ({ isOpen, handleClose, onAddTaskSucess }) => {
       },
       body: JSON.stringify(task),
     })
-    console.log(response, "fjdkfd")
 
     if (!response.ok) {
       return toast.error("Erro ao adicionar tarefa.")
     }
     onAddTaskSucess(task)
-    setIsLoading(false)
     handleClose()
+    reset()
   }
 
-  const titleError = errors.find((error) => error.inputName === "title")
-  const timeError = errors.find((error) => error.inputName === "time")
-  const descriptionError = errors.find(
-    (error) => error.inputName === "description"
-  )
   return (
     <CSSTransition
       nodeRef={nodeRef}
@@ -99,63 +73,88 @@ const AddTaskDialog = ({ isOpen, handleClose, onAddTaskSucess }) => {
             className="fixed bottom-0 left-0 top-0 flex h-screen w-screen items-center justify-center backdrop-blur"
           >
             {/* DIALOG */}
-            <div className="rounded-xl bg-white p-5 text-center shadow">
-              <h2 className="brand-dark-blue text-xl font-semibold">
-                Nova Tarefa
-              </h2>
-              <p className="mb-1 mt-1 text-sm text-brand-text-gray">
-                Insira as informações abaixo
-              </p>
+            <form onSubmit={handleSubmit(handleSaveClick)} action="">
+              <div className="rounded-xl bg-white p-5 text-center shadow">
+                <h2 className="brand-dark-blue text-xl font-semibold">
+                  Nova Tarefa
+                </h2>
+                <p className="mb-1 mt-1 text-sm text-brand-text-gray">
+                  Insira as informações abaixo
+                </p>
 
-              <div className="flex w-[336px] flex-col space-y-4">
-                <Input
-                  id="title"
-                  label="Título"
-                  placeholder="Insira o título da tarefa"
-                  errorMessage={titleError?.message}
-                  ref={titleRef}
-                  disabled={isLoading}
-                />
-                <TimeSelect
-                  value={time}
-                  onChange={(e) => setTime(e.target.value)}
-                  errorMessage={timeError?.message}
-                  disabled={isLoading}
-                />
+                <div className="flex w-[336px] flex-col space-y-4">
+                  <Input
+                    id="title"
+                    label="Título"
+                    placeholder="Insira o título da tarefa"
+                    {...register("title", {
+                      required: "Título é obrigatório",
+                      minLength: {
+                        value: 3,
+                        message: "Título deve ter no mínimo 3 caracteres",
+                      },
+                      validate: (value) => {
+                        if (value.trim().length === 0) {
+                          return "Título não pode ser vazio"
+                        }
+                        return true
+                      },
+                    })}
+                    errorMessage={errors?.title?.message}
+                    disabled={isSubmitting}
+                  />
+                  <TimeSelect
+                    value={time}
+                    onChange={(e) => setTime(e.target.value)}
+                    disabled={isSubmitting}
+                  />
 
-                <Input
-                  id="description"
-                  label="Descrição"
-                  placeholder="Descreva a Tarefa"
-                  errorMessage={descriptionError?.message}
-                  ref={descriptionRef}
-                  disabled={isLoading}
-                />
+                  <Input
+                    id="description"
+                    label="Descrição"
+                    placeholder="Descreva a Tarefa"
+                    {...register("description", {
+                      required: "Descrição é obrigatório",
+                      minLength: {
+                        value: 3,
+                        message: "Descrição deve ter no mínimo 3 caracteres",
+                      },
+                      validate: (value) => {
+                        if (value.trim().length === 0) {
+                          return "Descrição não pode ser vazia"
+                        }
+                        return true
+                      },
+                    })}
+                    errorMessage={errors?.description?.message}
+                    disabled={isSubmitting}
+                  />
 
-                <div className="flex justify-center gap-4">
-                  <Button
-                    color="secondary"
-                    className="w-full justify-center"
-                    size="large"
-                    onClick={handleClose}
-                  >
-                    Cancelar
-                  </Button>
-                  {isLoading ? (
-                    <Loader2Icon className="animate-spin text-brand-text-gray" />
-                  ) : (
+                  <div className="flex justify-center gap-4">
                     <Button
+                      color="secondary"
                       className="w-full justify-center"
                       size="large"
-                      onClick={handleSaveClick}
-                      disabled={isLoading}
+                      onClick={handleClose}
                     >
-                      Adicionar
+                      Cancelar
                     </Button>
-                  )}
+                    {isSubmitting ? (
+                      <Loader2Icon className="animate-spin text-brand-text-gray" />
+                    ) : (
+                      <Button
+                        className="w-full justify-center"
+                        type="submit"
+                        size="large"
+                        disabled={isSubmitting}
+                      >
+                        Adicionar
+                      </Button>
+                    )}
+                  </div>
                 </div>
               </div>
-            </div>
+            </form>
           </div>,
           document.body
         )}
