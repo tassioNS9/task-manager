e98032b5e5f57bfea85526c11ddc044fd844c764
Tassio Neves <tassions@hotmail.com>
2026-02-05 14:25:51 -0300

    refactor: add task mutation keys variable

--- src/hooks/data/use-delete-tasks.js
+++ src/hooks/data/use-delete-tasks.js
@@ -1,12 +1,13 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 
+import { taskMutationKeys } from "../../keys/mutations"
 import { taskQueryKeys } from "../../keys/queries"
 import { api } from "../../lib/axios"
 
 export const useDeleteTasks = (taskId) => {
   const queryClient = useQueryClient()
   return useMutation({
-    mutationKey: ["deleteTask", taskId],
+    mutationKey: taskMutationKeys.delete(taskId),
     mutationFn: async () => {
       const { data: deletedTask } = await api.delete(`/tasks/${taskId}`)
 
--- src/hooks/data/use-update-tasks.js
+++ src/hooks/data/use-update-tasks.js
@@ -1,11 +1,12 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 
+import { taskMutationKeys } from "../../keys/mutations"
 import { taskQueryKeys } from "../../keys/queries"
 import { api } from "../../lib/axios"
 export const useUpdateTasks = (taskId) => {
   const queryClient = useQueryClient()
   return useMutation({
-    mutationKey: ["updateTask", taskId],
+    mutationKey: taskMutationKeys.update(taskId),
     mutationFn: async (updatedFields) => {
       const { data: updatedTask } = await api.patch(
         `/tasks/${taskId}`,
